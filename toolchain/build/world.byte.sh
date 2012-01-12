433f28a78a848204033eee4fd5e0f1f6e3315fed
jessicah <jessica.l.hamilton@gmail.com>
2012-01-12 20:32:56 +1300

    changes not easily comparable; starting over

--- toolchain/build/world.byte.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# $Id: world.byte.sh 10526 2010-06-04 20:01:14Z maranget $
-set -e
-cd `dirname $0`/..
-. build/targets.sh
-set -x
-$OCAMLBUILD $@ \
-  $STDLIB_BYTE $OCAMLC_BYTE $OCAMLLEX_BYTE $OCAMLOPT_BYTE $TOPLEVEL $TOOLS_BYTE \
-  $OTHERLIBS_BYTE $OCAMLBUILD_BYTE $DEBUGGER $OCAMLDOC_BYTE $CAMLP4_BYTE
